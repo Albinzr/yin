806aeb0bb3d5b746327dea12bd855a72f6f568c2
Albin CR <Albinzr@gmail.com>
2020-04-26 11:40:54 +0530

    removed file name logging

--- src/server.go
+++ src/server.go
@@ -137,7 +137,6 @@ func readQueueCallback(message string, fileName string) {
 	scanner := bufio.NewScanner(strings.NewReader(message))
 	for scanner.Scan() {
 		msg := scanner.Text()
-		util.LogInfo(fileName)
 		kafkaConfig.Write(msg, func(isWritten bool) {
 			if !isWritten {
 				errCount++
